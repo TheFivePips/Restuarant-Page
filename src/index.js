41c0fc9b1a2ffa4e1e9aef7295432bcc530f3e64
sam gardner <samgardner20@gmail.com>
2022-08-15 14:07:12 -0400

    setting up webpack and starting with the home page html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log("hello from the src/index.js");
+import './style.css';
+import Icon from './forknknife.png';
+import BackgroundImg from './restaurant.jpg';
+
+const contentDiv = document.getElementById("content")
+
+const myIcon = new Image();
+myIcon.src = Icon;
+myIcon.classList.add('icon')
+
+const restImg = new Image()
+restImg.src = BackgroundImg;
+
+const nav = document.createElement('nav')
+nav.classList.add('navbar')
+nav.appendChild(myIcon)
+
+const navlinksUL = document.createElement('ul')
+navlinksUL.classList.add('navUL')
+navlinksUL.innerHTML= `
+<li><a href=''>Home</a></li>
+<li><a href=''>Contact</a></li>
+<li><a href=''>Menu</a></li>`
+
+nav.appendChild(navlinksUL)
+
+contentDiv.appendChild(nav)
