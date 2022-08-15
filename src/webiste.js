34c53d6c2d9130553317c03be825acac26cd5a27
sam gardner <samgardner20@gmail.com>
2022-08-15 14:59:03 -0400

    made the base website file that will load all the other content files

--- src/webiste.js
+++ src/webiste.js
@@ -1,37 +1,90 @@
 // this is the base website js page. It will include a header with nav links, a blank body(where the other content will be loaded by other files), and a footer.it will export a function to initialize the website to the index.js file
 
-function header() {
-    const nav = document.createElement('nav')
-    nav.classList.add('navbar')
+function createHeader() {
+    const header = document.createElement('header')
+    header.classList.add('header')
 
     const myIcon = new Image();
     myIcon.src = Icon;
     myIcon.classList.add('icon')
 
+    const name = document.createElement('h1')
+    name.textContent = "Tella's Tacos"
 
-    nav.appendChild(myIcon)
+    
+    header.appendChild(myIcon)
+    header.appendChild(name)
+    header.appendChild(createNav())
+    
+
+    return header
 
 }
 
-import './style.css';
-import Icon from './forknknife.png';
-import BackgroundImg from './restaurant.jpg';
+function createNav(){
 
-const contentDiv = document.getElementById("content")
+    const nav = document.createElement('nav')
+    nav.classList.add('nav')
+    
+    // each of these btns will need an event listener that loads the appropriate js file and marks the page as active
+    const homebtn = document.createElement('button')
+    homebtn.classList.add('homebtn')
+    homebtn.textContent = 'Home'
+    homebtn.addEventListener("click", function(event) {
+        // check for if it is the active page
+        // load home content function here
+    })
 
+    const contactbtn = document.createElement('button')
+    contactbtn.classList.add('contactbtn')
+    contactbtn.textContent = 'Contact'
+    contactbtn.addEventListener("click", function(event) {
+        // check for if it is the active page
+        // load home content function here
+    })
 
+    const menubtn = document.createElement('button')
+    menubtn.classList.add('menubtn')
+    menubtn.textContent = 'Menu'
+    menubtn.addEventListener("click", function(event) {
+        // check for if it is the active page
+        // load home content function here
+    })
 
-const restImg = new Image()
-restImg.src = BackgroundImg;
+    nav.appendChild(homebtn)
+    nav.appendChild(contactbtn)
+    nav.appendChild(menubtn)
 
+    
+}
 
-const navlinksUL = document.createElement('ul')
-navlinksUL.classList.add('navUL')
-navlinksUL.innerHTML= `
-<li><a href=''>Home</a></li>
-<li><a href=''>Contact</a></li>
-<li><a href=''>Menu</a></li>`
+function activeBtn () {
+    // not defined yet
+    // loops through the nav buttons and sets the active class
+}
 
-nav.appendChild(navlinksUL)
+function createMain() {
+    const main = document.createElement("main");
+    main.classList.add("main");
+    main.setAttribute("id", "main");
+    return main;
+}
 
-contentDiv.appendChild(nav)
+function createFooter() {
+    const footer = document.createElement("footer");
+    footer.classList.add("footer");
+//   maybe add some other things in here like an adresss, tm, etc
+  
+    return footer;
+}
+
+function initializeWebsite() {
+    // put all the static website elements(header,nav, main, footer) onto the page and then load the home page
+    const contentDiv = document.getElementById('content')
+
+    contentDiv.appendChild(createHeader())
+    contentDiv.appendChild(createMain())
+    contentDiv.appendChild(createFooter())
+
+    // set the home page to active with activebtn()
+}
