0c86b77cf529d5072ccfd0b6b9f09efcec3781f1
sam gardner <samgardner20@gmail.com>
2022-08-17 12:12:14 -0400

    went back to a more simplified enviornment for now. still learning webpack

--- dist/main.js
+++ dist/main.js
@@ -1,32 +1,10 @@
-/*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
 /******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/index.js":
+var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
-
-eval("// this will be the file that initializes the website using the import function from the website js file\n// Write the tab-switching logic inside of index.js. You should have event listeners for each tab that wipes out the current contents and then runs the correct ‘tab module’ to populate it again.\n\n//# sourceURL=webpack://restuarant-page/./src/index.js?");
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
-/******/ 	
+// this will be the file that initializes the website using the import function from the website js file
+// Write the tab-switching logic inside of index.js. You should have event listeners for each tab that wipes out the current contents and then runs the correct ‘tab module’ to populate it again.
 /******/ })()
 ;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0Esa00iLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZXN0dWFyYW50LXBhZ2UvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gdGhpcyB3aWxsIGJlIHRoZSBmaWxlIHRoYXQgaW5pdGlhbGl6ZXMgdGhlIHdlYnNpdGUgdXNpbmcgdGhlIGltcG9ydCBmdW5jdGlvbiBmcm9tIHRoZSB3ZWJzaXRlIGpzIGZpbGVcbi8vIFdyaXRlIHRoZSB0YWItc3dpdGNoaW5nIGxvZ2ljIGluc2lkZSBvZiBpbmRleC5qcy4gWW91IHNob3VsZCBoYXZlIGV2ZW50IGxpc3RlbmVycyBmb3IgZWFjaCB0YWIgdGhhdCB3aXBlcyBvdXQgdGhlIGN1cnJlbnQgY29udGVudHMgYW5kIHRoZW4gcnVucyB0aGUgY29ycmVjdCDigJh0YWIgbW9kdWxl4oCZIHRvIHBvcHVsYXRlIGl0IGFnYWluLiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
--- webpack.common.js
+++ webpack.common.js
@@ -3,6 +3,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
+  output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+  },
+  devtool: 'inline-source-map',
   plugins: [
 
     new HtmlWebpackPlugin({
@@ -20,6 +27,10 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
